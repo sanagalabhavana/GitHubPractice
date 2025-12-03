1f8c13adc236021a6bf7ea254dc98a70e76e6456
llall <llall@sanagala12345>
2025-12-03 17:31:47 +0530

    My first commit

---
+++ ProjectForMavenGithubJenkinsPractice/src/test/java/package1/Class1Test.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class1Test {
+	
+	@Test
+	public void class1() {
+		Reporter.log(".........Hi shantha........",true);
+	}
+}
---
+++ ProjectForMavenGithubJenkinsPractice/src/test/java/package1/Class3Test.java
@@ -0,0 +1,12 @@
+package package1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Class3Test {
+	
+	@Test
+	public void class1() {
+		Reporter.log(".........Hi shantha........",true);
+	}
+}
